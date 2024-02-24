eb7308f0e7941132a26e10a854f56d2fdb00fbc9
rocke3 <rfmamun0@gmail.com>
2024-02-25 02:44:24 +0600

    as

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -4,6 +4,8 @@
 <div id="{{ $id }}" data-modal-backdrop="static" tabindex="-1" aria-hidden="true"
     class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
     <div class="relative p-4 w-full max-w-2xl max-h-full">
+        <span data-modal-hide="{{ $id }}"
+            class="material-symbols-outlined absolute cursor-pointer text-red-700 right-0 bg-white rounded-full">close</span>
         <!-- Modal content -->
         <div class="relative bg-white rounded-[50px] shadow border-[4px] border-[#8280af] p-6">
             {{ $slot }}
