4a679573f5f8a71e3189e96a32959d0d830328d9
rocke3 <rfmamun0@gmail.com>
2024-02-25 02:37:40 +0600

    asd

--- resources/views/components/privacy.blade.php
+++ resources/views/components/privacy.blade.php
@@ -1,2 +1,316 @@
-<p class=" text-2xl font-semibold text-center">Privacy Policy</p>
-<div class="min-h-40"></div>
+<p class=" text-2xl font-semibold text-center mb-4">Privacy Policy</p>
+<p>This Privacy Policy (&quot;Policy&quot;) applies to palmy,
+    and Palmy (&quot;Company&quot;) and governs data collection and usage. For the
+    purposes of this Privacy Policy, unless otherwise noted, all references to the
+    Company include palmy.io. The Company's application is a Fundraising &amp;
+    Donation application. By using the Company application, you consent to the data
+    practices described in this statement. </p>
+
+<p class="mt-3"><b style='mso-bidi-font-weight:normal'>Collection of your
+        Personal Information</b> </p>
+
+<p>In order to better provide you with products and services
+    offered, the Company may collect personally identifiable information, such as
+    your: </p>
+
+
+
+<p style='margin-left:.5in;text-indent:-.25in'>-First and last
+    name </p>
+
+<p style='margin-left:.5in;text-indent:-.25in'>-Mailing address
+</p>
+
+<p style='margin-left:.5in;text-indent:-.25in'>-Email address </p>
+
+<p style='margin-left:.5in;text-indent:-.25in'>-Phone number </p>
+
+
+
+<p>If you purchase the Company's products and services, we
+    collect billing and credit card information. This information is used to
+    complete the purchase transaction. </p>
+
+
+
+<p>We do not collect any personal information about you unless
+    you voluntarily provide it to us. However, you may be required to provide
+    certain personal information to us when you elect to use certain products or
+    services. These may include: (a) registering for an account; (b) entering a
+    sweepstakes or contest sponsored by us or one of our partners; (c) signing up
+    for special offers from selected third parties; (d) sending us an email
+    message; (e) submitting your credit card or other payment information when
+    ordering and purchasing products and services. To wit, we will use your
+    information for, but not limited to, communicating with you in relation to
+    services and/or products you have requested from us. We also may gather
+    additional personal or non-personal information in the future. </p>
+
+
+
+<p class="mt-3"><b style='mso-bidi-font-weight:normal'>Use of your Personal
+        Information </b></p>
+
+<p>The Company collects and uses your personal information in
+    the following ways:&nbsp; </p>
+
+
+
+<p style='margin-left:.5in;text-indent:-.25in'>-to operate and
+    deliver the services you have requested </p>
+
+<p style='margin-left:.5in;text-indent:-.25in'>-to provide you
+    with information, products, or services that you request from us </p>
+
+<p style='margin-left:.5in;text-indent:-.25in'>-to provide you
+    with notices about your account </p>
+
+<p style='margin-left:.5in;text-indent:-.25in'>-to carry out
+    the Company's obligations and enforce our rights arising from any contracts
+    entered between you and us, including for billing and collection </p>
+
+<p style='margin-left:.5in;text-indent:-.25in'>-to notify you
+    about changes to our palmy or any products or services we offer or provide
+    through it </p>
+
+<p style='margin-left:.5in;text-indent:-.25in'>-in any other
+    way we may describe when you provide the information </p>
+
+<p style='margin-left:.5in;text-indent:-.25in'>-for any other
+    purpose with your consent. </p>
+
+
+
+<p>The Company may also use your personally identifiable
+    information to inform you of other products or services available from the
+    Company and its affiliates. </p>
+
+
+
+<p class="mt-3"><b style='mso-bidi-font-weight:normal'>Sharing Information
+        with Third Parties</b> </p>
+
+<p>The Company does not sell, rent, or lease its customer lists
+    to third parties. </p>
+
+
+
+<p>The Company may, from time to time, contact you on behalf of
+    external business partners about a particular offering that may be of interest
+    to you. In those cases, your unique personally identifiable information (email,
+    name, address, phone number) is transferred to the third party. The Company may
+    share data with trusted partners to help perform statistical analysis, send you
+    email or postal mail, provide customer support, or arrange for deliveries. All
+    such third parties are prohibited from using your personal information except
+    to provide these services tothe Company, and they are required to maintain the
+    confidentiality of your information. </p>
+
+
+
+<p>The Company may disclose your personal information, without
+    notice, if required to do so by law or in the good faith belief that such
+    action is necessary to: (a) conform to the edicts of the law or comply with
+    legal process served on the Company or the site; (b) protect and defend the
+    rights or property of the Company; and/or (c) act under exigent circumstances
+    to protect the personal safety of users of the Company, or the public. </p>
+
+
+
+<p class="mt-3"><b style='mso-bidi-font-weight:normal'>Opt-Out of Disclosure
+        of Personal Information to Third Parties</b> </p>
+
+<p>In connection with any personal information we may disclose
+    to a third party for a business purpose, you have the right to know: </p>
+
+
+
+<p style='margin-left:.5in;text-indent:-.25in'>-The categories
+    of personal information that we disclosed about you for a business purpose. </p>
+
+
+
+<p>You have the right under the California Consumer Privacy Act
+    of 2018 (CCPA) and certain other privacy and data protection laws, as
+    applicable, to opt out of the disclosure of your personal information. If you
+    exercise your right to opt out of the disclosure of your personal information,
+    we will refrain from disclosing your personal information, unless you
+    subsequently provide express authorization for the disclosure of your personal
+    information. To opt out of the disclosure of your personal information, visit
+    this web page contact@palmy.io. </p>
+
+
+
+<p class="mt-3"><b style='mso-bidi-font-weight:normal'>Right to Deletion</b>
+</p>
+
+<p>Subject to certain exceptions set out below, on receipt of a
+    verifiable request from you, we will: </p>
+
+
+
+<p style='margin-left:.5in;text-indent:-.25in'>-Delete your
+    personal information from our records; and </p>
+
+<p style='margin-left:.5in;text-indent:-.25in'>-Direct any
+    service providers to delete your personal information from their records. </p>
+
+
+
+<p>Please note that we may not be able to comply with requests
+    to delete your personal information if it is necessary to: </p>
+
+
+
+<p style='margin-left:.5in;text-indent:-.25in'>-Complete the
+    transaction for which the personal information was collected, fulfill the terms
+    of a written warranty or product recall conducted in accordance with federal
+    law, and provide a good or service requested by you, or reasonably anticipated
+    within the context of our ongoing business relationship with you, or otherwise
+    perform a contract between you and us; </p>
+
+<p style='margin-left:.5in;text-indent:-.25in'>-Detect security
+    incidents, protect against malicious, deceptive, fraudulent, or illegal
+    activity; or prosecute those responsible for that activity; </p>
+
+<p style='margin-left:.5in;text-indent:-.25in'>-Debug to
+    identify and repair errors that impair existing intended functionality; </p>
+
+<p style='margin-left:.5in;text-indent:-.25in'>-Exercise free
+    speech, ensure the right of another consumer to exercise his or her right of
+    free speech, or exercise another right provided for by law; </p>
+
+<p style='margin-left:.5in;text-indent:-.25in'>-Comply with the
+    California Electronic Communications Privacy Act; </p>
+
+<p style='margin-left:.5in;text-indent:-.25in'>-Engage in
+    public or peer-reviewed scientific, historical, or statistical research in the
+    public interest that adheres to all other applicable ethics and privacy laws,
+    when our deletion of the information is likely to render impossible or
+    seriously impair the achievement of such research, provided we have obtained
+    your informed consent; </p>
+
+<p style='margin-left:.5in;text-indent:-.25in'>-Enable solely
+    internal uses that are reasonably aligned with your expectations based on your
+    relationship with us; </p>
+
+<p style='margin-left:.5in;text-indent:-.25in'>-Comply with an
+    existing legal obligation; or </p>
+
+<p style='margin-left:.5in;text-indent:-.25in'>-Otherwise use
+    your personal information, internally, in a lawful manner that is compatible
+    with the context in which you provided the information. </p>
+
+
+
+<p class="mt-3"><b style='mso-bidi-font-weight:normal'>Children Under
+        Thirteen</b> </p>
+
+<p>The Company does not knowingly collect personally
+    identifiable information from children under the age of 13. If you are under
+    the age of 13, you must ask your parent or guardian for permission to use this
+    application. </p>
+
+
+
+<p class="mt-3"><b style='mso-bidi-font-weight:normal'>Disconnecting your
+        Company's Account from Third Party Websites</b> </p>
+
+<p>You will be able to connect your Company's account to third-party
+    accounts. BY CONNECTING YOUR COMPANY'S ACCOUNT TO YOUR THIRD-PARTY ACCOUNT, YOU
+    ACKNOWLEDGE AND AGREE THAT YOU ARE CONSENTING TO THE CONTINUOUS RELEASE OF
+    INFORMATION ABOUT YOU TO OTHERS (IN ACCORDANCE WITH YOUR PRIVACY SETTINGS ON
+    THOSE THIRD-PARTY SITES). IF YOU DO NOT WANT INFORMATION ABOUT YOU, INCLUDING
+    PERSONALLY IDENTIFYING INFORMATION, TO BE SHARED IN THIS MANNER, DO NOT USE
+    THIS FEATURE. You may disconnect your account from a third-party account at any
+    time. Palmy </p>
+
+
+
+<p class="mt-3"><b style='mso-bidi-font-weight:normal'>Opt Out and Unsubscribe
+        from Third-Party Communications</b> </p>
+
+<p>We respect your privacy and give you an opportunity to opt
+    out of receiving announcements of certain information. Users may opt out of
+    receiving any or all communications from third-party partners of the Company by
+    contacting us here: </p>
+
+
+
+<p style='margin-left:.5in;text-indent:-.25in'>-Web page:
+    contact@palmy.io </p>
+
+<p style='margin-left:.5in;text-indent:-.25in'>-Email:
+    contact@palmy.io </p>
+
+<p style='margin-left:.5in;text-indent:-.25in'>-Phone:
+    2067527883 </p>
+
+
+
+<p class="mt-3"><b style='mso-bidi-font-weight:normal'>Email Communications</b>
+</p>
+
+<p>From time to time, the Company may contact you via email for
+    the purpose of providing announcements, promotional offers, alerts, confirmations,
+    surveys, and/or other general communication. </p>
+
+
+
+<p>If you would like to stop receiving marketing or promotional
+    communications via email from the Company, you may opt out of such
+    communications by replying STOP or clicking on the unsubscribe button. </p>
+
+
+
+<p><b style='mso-bidi-font-weight:normal'>External Data Storage
+        Sites</b> </p>
+
+<p>We may store your data on servers provided by third-party
+    hosting vendors with whom we have contracted. </p>
+
+
+
+<p class="mt-3"><b style='mso-bidi-font-weight:normal'>Changes to This
+        Statement</b> </p>
+
+<p>The Company reserves the right to change this Policy from
+    time to time. For example, when there are changes in our services, changes in
+    our data protection practices, or changes in the law. When changes to this
+    Policy are significant, we will inform you. You may receive a notice by sending
+    an email to the primary email address specified in your account, by placing a
+    prominent notice on our Palmy, and/or by updating any privacy information. Your
+    continued use of the application and/or services available after such
+    modifications will constitute your: (a) acknowledgment of the modified Policy;
+    and (b) agreement to abide and be bound by that Policy. </p>
+
+
+
+<p class="mt-3"><b style='mso-bidi-font-weight:normal'>Contact Information</b>
+</p>
+
+<p>The Company welcomes your questions or comments regarding
+    this Policy. If you believe that the Company has not adhered to this Policy,
+    please contact the Company at: </p>
+
+
+
+<p>Palmy </p>
+
+<p>300 Lenora Street 6135 </p>
+
+<p>Seattle, Washington 98101 </p>
+
+
+
+<p>Email Address: </p>
+
+<p>contact@palmy.io </p>
+
+
+
+<p>Phone Number: </p>
+
+<p>2067527883 </p>
+
+
+
+<p>Effective as of February 24, 2024 </p>
